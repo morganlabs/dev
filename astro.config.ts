41aeb75de08c8c7a7479b34927ca41b5841a3631
Morgan Jones <me@morganlabs.dev>
2024-12-10 15:31:44 +0000

    feat: colors

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,18 @@
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
-export default defineConfig({});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "${__dirname}/src/styles/vars" as *;\n`,
+        },
+      },
+    },
+  },
+});
